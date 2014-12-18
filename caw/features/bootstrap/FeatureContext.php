712df985c1ff8259dd17915695100aa44cf1cde6
John Bickar <jbickar@stanford.edu>
2014-12-18 12:52:06 -0800

    Updating FeatureContext.php for CAW

--- caw/features/bootstrap/FeatureContext.php
+++ caw/features/bootstrap/FeatureContext.php
@@ -3,9 +3,12 @@
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\TranslatedContextInterface,
     Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\PendingException;
+    Behat\Behat\Exception\PendingException,
+    Behat\Mink\Exception\ExpectationException,
+    Behat\Mink\Session;
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
+use Drupal\Component\Utility\Random;
 
 //
 // Require 3rd-party libraries here:
@@ -17,7 +20,7 @@ use Behat\Gherkin\Node\PyStringNode,
 /**
  * Features context.
  */
-// class FeatureContext extends BehatContext 
+// class FeatureContext extends BehatContext
 class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext
 {
     /**
@@ -31,31 +34,32 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext
         // Initialize your context here
     }
 
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
-
-  /**
-   * @Given /^I wait (\d+) seconds$/
-   */
-  public function iWaitSeconds($seconds) {
-    $this->getSession()->wait(1000*$seconds);
-  }
 
   /**
    * @Given /^the "([^"]*)" module is enabled$/
    */
   public function theModuleIsEnabled($arg1) {
-    $this->invoke_code('module_enable', array('array(\'$arg1\')'), TRUE);
+    if(!$this->invoke_code('module_enable', array('array("'. $arg1 . '")'), TRUE)) {
+      throw new PendingException();
+    }
   }
+
+  /**
+   * @Given /^the "([^"]*)" module is disabled$/
+   */
+  public function theModuleIsDisabled($arg1) {
+    if(!$this->invoke_code('module_disable', array('array("'. $arg1 . '")'), TRUE)) {
+      throw new PendingException();
+    }
+  }
+
+  /**
+   * @Given /^I wait (\d+) second(s)?$/
+   */
+  public function iWaitSeconds($seconds) {
+    $this->getSession()->wait(1000*$seconds);
+  }
+
   /**
   * Invoking a php code with drush.
   *
@@ -77,6 +81,172 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext
     }
 
     return $output;
+  }
+
+  /**
+   * Override DrupalContext::login
+   * The WebAuth Module for Drupal (WMD) hides the user login form in a collapsible fieldset.
+   * We need to open that fieldset up to be able to fill out the fields
+   */
+  public function login() {
+    // Check if logged in.
+    if ($this->loggedIn()) {
+      $this->logout();
+    }
+
+    if (!$this->user) {
+      throw new \Exception('Tried to login without a user.');
+    }
+
+    $this->getSession()->visit($this->locatePath('/user'));
+    $element = $this->getSession()->getPage();
+    // find the Local User Login link - it's only findable in the browser, with Javascript
+    // See Behat\Mink\Element\TraversableElement::findLink
+    $localuserlogin = $element->findLink("Local User Login");
+    if (!is_null($localuserlogin)) {
+      // click on the Local User Login link to expose the user name and password fields
+      // See Behat\Mink\Element\TraversableElement::clickLink
+      $element->clickLink("Local User Login");
+    }
+    $element->fillField($this->getDrupalText('username_field'), $this->user->name);
+    $element->fillField($this->getDrupalText('password_field'), $this->user->pass);
+    $submit = $element->findButton($this->getDrupalText('log_in'));
+    if (empty($submit)) {
+      throw new \Exception(sprintf("No submit button at %s", $this->getSession()->getCurrentUrl()));
+    }
+
+    // Log in.
+    $submit->click();
+
+    if (!$this->loggedIn()) {
+      throw new \Exception(sprintf("Failed to log in as user '%s' with role '%s'", $this->user->name, $this->user->role));
+    }
+  }
+
+  /**
+   * Override MinkContext::fixStepArgument().
+   *
+   * Make it possible to use [random].
+   * If you want to use the previous random value [random:1].
+   * See http://cgit.drupalcode.org/panopoly/tree/tests/behat/features/bootstrap/FeatureContext.php?id=18a2ccbdad8c8064aa36f8c57ae7416ee018b92f
+   */
+  protected function fixStepArgument($argument) {
+    $argument = str_replace('\\"', '"', $argument);
+
+    // Token replace the argument.
+    static $random = array();
+    for ($start = 0; ($start = strpos($argument, '[', $start)) !== FALSE; ) {
+      $end = strpos($argument, ']', $start);
+      if ($end === FALSE) {
+        break;
+      }
+      $name = substr($argument, $start + 1, $end - $start - 1);
+      if ($name == 'random') {
+        $randomname = $this->getDrupal()->random->name(8);
+        $randomname = strtolower($randomname);
+        $this->vars[$name] = $randomname;
+        $random[] = $this->vars[$name];
+      }
+      // In order to test previous random values stored in the form,
+      // suppport random:n, where n is the number or random's ago
+      // to use, i.e., random:1 is the previous random value.
+      elseif (substr($name, 0, 7) == 'random:') {
+        $num = substr($name, 7);
+        if (is_numeric($num) && $num <= count($random)) {
+          $this->vars[$name] = $random[count($random) - $num];
+        }
+      }
+      if (isset($this->vars[$name])) {
+        $argument = substr_replace($argument, $this->vars[$name], $start, $end - $start + 1);
+        $start += strlen($this->vars[$name]);
+      }
+      else {
+        $start = $end + 1;
+      }
+    }
+
+    return $argument;
+  }
+
+  /**
+   * @Then /^I should see (\d+) or more "([^"]*)" elements$/
+   */
+  public function iShouldSeeOrMoreElements($num, $element) {
+
+    $container = $this->getSession()->getPage();
+    $nodes = $container->findAll('css', $element);
+
+    if (intval($num) > count($nodes)) {
+      $session = $this->getSession();
+      $message = sprintf('%d "%s" elements found when there should be a minimum of %d.', count($nodes), $element, $num);
+      throw new ExpectationException($message, $session);
+    }
+
+  }
+
+    /**
+   * @Then /^I should see (\d+) or fewer "([^"]*)" elements$/
+   */
+  public function iShouldSeeOrFewerElements($num, $element) {
+    $container = $this->getSession()->getPage();
+    $nodes = $container->findAll('css', $element);
+
+    if (intval($num) < count($nodes)) {
+      $session = $this->getSession();
+      $message = sprintf('%d "%s" elements found when there should be a maximum of %d.', count($nodes), $element, $num);
+      throw new ExpectationException($message, $session);
+    }
+  }
+
+  /**
+   * @When /^I hover over the element "([^"]*)"$/
+   */
+  public function iHoverOverTheElement($locator) {
+    $session = $this->getSession(); // get the mink session
+    $element = $session->getPage()->find('css', $locator); // runs the actual query and returns the element
+
+    // errors must not pass silently
+    if (null === $element) {
+      throw new \InvalidArgumentException(sprintf('Could not evaluate CSS selector: "%s"', $locator));
+    }
+
+    // ok, let's hover it
+    $element->mouseOver();
+  }
+
+  /**
+   * Click some text
+   *
+   * @When /^I click on the text "([^"]*)"$/
+   */
+  public function iClickOnTheText($text) {
+    $session = $this->getSession();
+    $element = $session->getPage()->find(
+      'xpath',
+      $session->getSelectorsHandler()->selectorToXpath('xpath', '*//*[text()="'. $text .'"]')
+    );
+    if (null === $element) {
+      throw new \InvalidArgumentException(sprintf('Cannot find text: "%s"', $text));
+    }
+
+    $element->click();
+  }
+
+  /**
+   * Find the default value of a select element.
+   * See https://github.com/Behat/Mink/issues/300
+   * @Then /^I want to validate select field option "([^"]*)" default is "([^"]*)"$/
+   */
+  public function iWantToValidateSelectOptionDefaultIs($locator, $defaultValue) {
+       $optionElement = $this->getSession()->getPage()->find('xpath', '//select[@name="' . $locator . '"]/option[@selected]');
+       if (!$optionElement) {
+          throw new Exception('Could not find a select element with the "name" attribute of ' . $locator);
+       }
+
+      $selectedDefaultValue = (string)$optionElement->getText();
+       if ($selectedDefaultValue != $defaultValue) {
+          throw new Exception('Select option default value: "' . $selectedDefaultValue . '" does not match given: "' . $defaultValue . '"');
+       }
   }
 
 }
